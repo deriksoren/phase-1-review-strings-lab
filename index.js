650c931ec1f93f62f6c1cb9ad2df8bd0cef7934a
Erik Sorensen <deriksoren@gmail.com>
2023-04-05 23:31:01 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Erik';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase ();
+const shortGreeting = `Welcome, ${currentUser [0]} !`;
